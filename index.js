b068e7fe4877d4be652ec7967bc2b95fb7615f13
Adam Merry <adammerry44@gmail.com>
2016-06-03 12:04:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,4 @@ $(document).ready(function(){
         $('img#richguy').fadeTo("slow",1);
         $("img#richguy").slideUp("fast");
     });
-    $("nav").mouseenter(function(){
-        $(this).animate({
-            height: '+=10px'
-    });
-    $("nav").mouseleave(function(){
-        $(this).animate({
-            height: "-=10px"
-    });
 });
