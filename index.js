41b0cc8ed93a15a24264452440eec5c563dd4a7f
Adam Merry <adammerry44@gmail.com>
2016-06-03 10:21:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-alert("This is an alert I created in index.js!");
+$(document).ready(fucntion(){
+    $("img#richguy").slideDown("slow");
+    $('img#richguy').mouseenter(function() {
+        $('img#richguy').fadeTo("fast",0.5);
+    });
+    $("img#richguy").mouseleave(function() {
+        $('img#richguy').fadeTo("fast",1);
+    });
+});
