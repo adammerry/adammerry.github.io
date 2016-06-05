6378106b238f3fe10f308a8d4d97a42c1fcb17b3
Adam Merry <adammerry44@gmail.com>
2016-06-05 17:14:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
         $('img#richguy').fadeTo("slow",1);
     });
     $("#paragraph").click(function(){
-        $("#paragraph").slideDown("fast")
+        $("this").slideDown("fast")
     });
     $('button').click(function() {
         $('.vanish').fadeOut("slow")
