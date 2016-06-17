9a927de2eaa83146af27ead68022953e3a6cf12f
Adam Merry <adammerry44@gmail.com>
2016-06-17 13:11:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ $(document).ready(function(){
     $("img#dynamite").click(function(){
         $(this).effect("explode");
     });
-        var slidewidth = $(document).width() / 2.5;
+        var slidewidth = $(window).width();
 
     function goRight(){
         $("#snake").animate({left: slidewidth}, 5000, function(){
