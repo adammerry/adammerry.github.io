0fecb1385140450e4df1752d1416aa90692588c2
Adam Merry <adammerry44@gmail.com>
2016-06-21 14:05:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -93,8 +93,21 @@ $(document).ready(function(){
       });
     }
     setTimeout(goRight, 50);
-    
-    var d = new Date("03/25/2015");
-    document.getElementById("date").innerHTML = d.toDateString();
-});
+
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if(dd<10) {
+        dd="0"+dd
+    } 
+
+    if(mm<10) {
+    mm="0"+mm
+    } 
+
+    today = mm+"/"+dd+"/"+yyyy;
+    document.getElementById("date").innerHTML = today;
+    });
 
