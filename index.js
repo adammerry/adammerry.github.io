ad284518637cb81f018c12e95089337e0bb8fffc
Adam Merry <adammerry44@gmail.com>
2016-06-21 14:44:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -98,7 +98,16 @@ $(document).ready(function(){
     var dd = today.getDate();
     var mm = today.getMonth()+1; //January is 0!
     var yyyy = today.getFullYear();
-
+    var weekday = new Array(7);
+        weekday[0] = "Sunday";
+        weekday[1] = "Monday";
+        weekday[2] = "Tuesday";
+        weekday[3] = "Wednesday";
+        weekday[4] = "Thursday";
+        weekday[5] = "Friday";
+        weekday[6] = "Saturday";
+        var dayofweek = weekday[today.getDay()]; 
+        
     if(dd<10) {
         dd="0"+dd
     } 
@@ -107,7 +116,7 @@ $(document).ready(function(){
     mm="0"+mm
     } 
 
-    today = mm+"/"+dd+"/"+yyyy;
+    today = dayofweek+", "+mm+"/"+dd+"/"+yyyy;
     document.getElementById("date").innerHTML = today;
     });
 
