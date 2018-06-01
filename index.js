cd7efc07ab0a44b41890272f96d99e2b8acec70d
Adam Merry <adammerry44@gmail.com>
2018-05-31 21:43:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 $(document).ready(function(){
-    $('img#richguy').mouseenter(function(){
-        $('img#richguy').fadeTo("slow",0);
-    });
-    $("img#richguy").mouseleave(function(){
-        $('img#richguy').fadeTo("slow",1);
-    });
     $('button').click(function(){
         $('.vanish').fadeOut("slow");
     });
